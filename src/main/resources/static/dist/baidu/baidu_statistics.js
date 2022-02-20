fbccf1bdf56cbaa411d803646c55cbb531651b73
wangyi15124 <wiaini83321394>
2022-02-20 18:44:30 +0800

    集成站点统计

---
+++ src/main/resources/static/dist/baidu/baidu_statistics.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?f17e264e40b4ac75672813c1a2ae7110";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
